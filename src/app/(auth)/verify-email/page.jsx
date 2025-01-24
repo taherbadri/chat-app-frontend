15cb3e81ee2826a241ffaa922b34ddcebe555120
taherbadri <badri.taher@yahoo.com>
2025-01-24 14:40:06 +0530

    fixed suspense wrapping bug for verif-email page

--- src/app/(auth)/verify-email/page.jsx
+++ src/app/(auth)/verify-email/page.jsx
@@ -4,21 +4,32 @@ import { verifyEmail } from '@/lib/features/authentication/authenticationSlice';
 import { useAppDispatch } from '@/lib/hooks';
 import { useRouter, useSearchParams } from 'next/navigation';
 import { useEffect } from 'react';
+import { Suspense } from 'react';
 
-export default function VerifyEmailPage() {
+function VerifyEmailContent() {
 	const email = useSearchParams().get('email');
 	const verificationToken = useSearchParams().get('verificationToken');
 	const router = useRouter();
 	const dispatch = useAppDispatch();
+
 	useEffect(() => {
 		if (email && verificationToken) {
 			dispatch(verifyEmail({ email, verificationToken }));
 		}
 		router.push('/login');
 	}, [dispatch, email, verificationToken, router]);
+
 	return (
 		<div className="flex min-h-svh flex-col items-center justify-center bg-muted p-6 md:p-10">
 			<div className="w-full max-w-sm md:max-w-3xl"></div>
 		</div>
+	);
+}
+
+export default function VerifyEmailPage() {
+	return (
+		<Suspense fallback={<div>Loading...</div>}>
+			<VerifyEmailContent />
+		</Suspense>
 	);
 }
